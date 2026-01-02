c7e9a626fe1fd8edf00a881e2f1808759fb939b3
RomanPR <prikhodjko.roman@gmail.com>
2026-01-02 14:47:23 +0200

    app dev start

--- App/zvitplus.app/src/main.tsx
+++ App/zvitplus.app/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from './app/providers/RouterProvider';
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider />
     </StrictMode>,
 )
---
+++ App/zvitplus.app/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,3 @@
+export function NotFoundPage() {
+    return <h1>404 — Page not found</h1>;
+}
